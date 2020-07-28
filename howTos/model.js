453858a289ce11fa96e78dea3ad94c2ca5df5760
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-07-27 18:32:45 -0700

    building howTo and review model/routers

---
+++ howTos/model.js
@@ -0,0 +1,37 @@
+const db = require("../data/dbconfig");
+
+async function addHowTo(howTo) {
+  const [id] = await db("howTos").insert(howTo);
+
+  return db("howTos").where({ id }).first();
+}
+
+function findHowTos() {
+  return db("howTos as h").select("h.id", "h.title", "h.user_id");
+}
+
+function findHowToBy(filter) {
+  return db("howTos").where(filter).first();
+}
+
+function findHowToById(id) {
+  return db("howTos").where({ id }).first();
+}
+
+function removeHowTo(id) {
+  return db("howTos").where({ id }).del();
+}
+
+async function updateHowTo(changes, id) {
+  await db("howTos").where({ id }).update(changes);
+  return db("howTos").where({ id }).first();
+}
+
+module.exports = {
+  addHowTo,
+  findHowTos,
+  findHowToBy,
+  findHowToById,
+  removeHowTo,
+  updateHowTo,
+};
