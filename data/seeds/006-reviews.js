dc23574b11d5b9bb0b0c30757b327080961d0838
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-07-29 16:24:52 -0700

    fixed err in review seed

--- data/seeds/006-reviews.js
+++ data/seeds/006-reviews.js
@@ -1,5 +1,5 @@
 exports.seed = async function (knex) {
-  await knex("howTos").insert([
+  await knex("reviews").insert([
     { id: 1, content: "This was terrible", howTos_id: 1, user_id: 1 },
     { id: 2, content: "Came out great!", howTos_id: 2, user_id: 2 },
     { id: 3, content: "Got sick immediately", howTos_id: 3, user_id: 3 },
